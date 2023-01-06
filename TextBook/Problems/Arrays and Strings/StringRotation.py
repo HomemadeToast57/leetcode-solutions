8f496b83ecac9ac1866b19a3e0bcd01c7a486fa1
HomemadeToast <54961512+HomemadeToast57@users.noreply.github.com>
2023-01-06 02:43:55 -0600

    Final commit to repo

--- TextBook/Problems/Arrays and Strings/StringRotation.py
+++
@@ -1,7 +0,0 @@
-def StringRotation(x1, x2):
-    str = x2 + x2
-    return str.find(x1) != -1
-
-print(StringRotation("waterbottle", "erbottlewat")) # True
-print(StringRotation("Test", "est")) # False
-print(StringRotation("CodingInterview", "erviewCodingInt")) # True
